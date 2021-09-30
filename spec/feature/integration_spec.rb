7e70a017cfa108b65e59ce3a827ae086500eced7
root <root@localhost>
2021-09-30 04:31:15 +0000

    Removed 'This is a test version' from the home page. Added integration testing for author, price, and published date.

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe 'Creating a book', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+
+  # scenario 'invalid inputs' do
+  #   visit new_book_path
+  #   fill_in 'Title', with: ''
+  #   click_on 'Create Book'
+  #   expect(page).to have_content("Title can't be blank")
+  # end
+
+  # test for new attributes listed in step 6, bullet 2
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K Rowling'
+    fill_in 'Price', with: '16.99'
+    fill_in 'Published Date', with: 'Sat, 10 Nov 2007'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+  end
+
 end
